a7d6aa93b20d919636781a1890b827b51335424a
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2022-05-11 12:58:45 -0300

    backward adaptive coding more Ns

--- scripts/backward_adaptive_coding.py
+++ scripts/backward_adaptive_coding.py
@@ -307,19 +307,23 @@ def backward_adaptive_coding_experiment(exp_name,docs,Ns,learning_rates):
 
 if __name__ == "__main__":
 
-    exp_name = "Adaptive_Detection_of_Dim_5pages_lr1e-2"
+    exp_name = "SPL2021_last_10_sorted_pages_lr1e-2"
 
-    docs = [ # docs[i,j] = the path to the j'th page from the i'th document
-        [
-            "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_1.png",
-            "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_2.png",
-            "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_3.png",
-            "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_4.png",
-            "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_5.png"
-        ]
-    ]
+    # exp_name = "Adaptive_Detection_of_Dim_5pages_lr1e-2"
 
-    Ns = [26] # [0,2,4,10,26,67,170]
+    # docs = [ # docs[i,j] = the path to the j'th page from the i'th document
+    #     [
+    #         "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_1.png",
+    #         "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_2.png",
+    #         "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_3.png",
+    #         "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_4.png",
+    #         "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_5.png"
+    #     ]
+    # ]
+
+    docs = [[os.path.join('/home/lucas/Documents/data/SPL2021/pages',f)] for f in sorted(os.listdir('/home/lucas/Documents/data/SPL2021/pages'))[-10:]]
+
+    Ns = [107] # [0,2,4,10,26,67,170] # [26,33,42,53,67,84,107,135,170]
     
     learning_rates = [0.01] # (3.162277659**np.array([-2,-4,-8]))
 
