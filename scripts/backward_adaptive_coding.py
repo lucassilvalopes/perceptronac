1c2daf2f3e1823dfe3d1b1c9dfd3605b42927d8c
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2022-05-14 17:07:56 -0300

    improving backward adaptive coding code organization

--- scripts/backward_adaptive_coding.py
+++ scripts/backward_adaptive_coding.py
@@ -30,6 +30,31 @@ from perceptronac.loading_and_saving import save_values
 from tqdm import tqdm
 
 
+# https://matplotlib.org/3.5.0/gallery/lines_bars_and_markers/linestyles.html
+linestyle_str = {
+     'solid': 'solid',      # Same as (0, ()) or '-'
+     'dotted': 'dotted',    # Same as (0, (1, 1)) or ':'
+     'dashed': 'dashed',    # Same as '--'
+     'dashdot': 'dashdot'   # Same as '-.'
+}
+
+linestyle_tuple = {
+     'loosely dotted':        (0, (1, 10)),
+     'dotted':                (0, (1, 1)),
+     'densely dotted':        (0, (1, 1)),
+     'loosely dashed':        (0, (5, 10)),
+     'dashed':                (0, (5, 5)),
+     'densely dashed':        (0, (5, 1)),
+     'loosely dashdotted':    (0, (3, 10, 1, 10)),
+     'dashdotted':            (0, (3, 5, 1, 5)),
+     'densely dashdotted':    (0, (3, 1, 1, 1)),
+     'dashdotdotted':         (0, (3, 5, 1, 5, 1, 5)),
+     'loosely dashdotdotted': (0, (3, 10, 1, 10, 1, 10)),
+     'densely dashdotdotted': (0, (3, 1, 1, 1, 1, 1))
+}
+
+
+
 class RNG:
     """
     16 bit taps at [16,15,13,4] Fibonacci LFSR
@@ -100,7 +125,7 @@ def initialize_MLP_N_64N_32N_1(model):
                     # torch.tensor(rand_vec(rng,-stdv,stdv,fan_out))
 
 
-class RealTimeCABAC:
+class RealTimeLUT:
 
     def __init__(self,N,central_tendency="mode"):
         self.po2 = 2 ** np.arange(0,N).reshape(-1,1)
@@ -118,13 +143,13 @@ class RealTimeCABAC:
         raise ValueError(f"{self.central_tendency} not an option, only \"mode\" or \"mean\"")
 
     def update(self,Xi,yi):
-        context = Xi @ self.po2
+        context = Xi @ self.po2 # np.array([[]]) @ (2 ** np.arange(0,0).reshape(-1,1)) == 0
         if (yi[0,0] == 1):
             self.c1[context[0,0],0] = self.c1[context[0,0],0] + 1
         else:
             self.c0[context[0,0],0] = self.c0[context[0,0],0] + 1
     def predict(self,Xi):
-        context = Xi @ self.po2
+        context = Xi @ self.po2 # np.array([[]]) @ (2 ** np.arange(0,0).reshape(-1,1)) == 0
         c1 = self.c1[context[0,0],0]
         c0 = self.c0[context[0,0],0]
 
@@ -141,51 +166,16 @@ class RealTimeCABAC:
             self.bad_central_tendency()
         return pp
 
-class RealTimeAC:
-    def __init__(self,central_tendency="mode"):
-        self.central_tendency = central_tendency
-        if self.central_tendency=="mode":
-            self.c1 = 0
-            self.c0 = 0
-        elif self.central_tendency=="mean":
-            self.c1 = 1
-            self.c0 = 1
-        else:
-            self.bad_central_tendency() 
-    def bad_central_tendency(self):
-        raise ValueError(f"{self.central_tendency} not an option, only \"mode\" or \"mean\"")
-    def update(self,yi):
-        if (yi[0,0] == 1):
-            self.c1 = self.c1 + 1
-        else:
-            self.c0 = self.c0 + 1
-    def predict(self):
-        pp = np.zeros((1,1)) 
-        if self.central_tendency == "mode":
-            pp[0,0] = np.max([1,self.c1]) / (np.max([1,self.c1]) + np.max([1,self.c0]))
-            if self.c0 != 0 and self.c1 == 0:
-                pp[0,0]=(0 + np.finfo(pp.dtype).eps)
-            elif self.c0 == 0 and self.c1 != 0:
-                pp[0,0]=(1 - np.finfo(pp.dtype).eps)
-        elif self.central_tendency=="mean":
-            pp[0,0] = self.c1 / (self.c1 + self.c0)
-        else:
-            self.bad_central_tendency()
-        return pp
 
-def backward_adaptive_coding(pths,N,lr,with_cabac=False,with_mlp=True):
+def backward_adaptive_coding(pths,N,lr,central_tendencies,with_lut=False,with_mlp=True):
     
 
     trainset = CausalContextDataset(pths,"image",N)
     y,X = trainset.y,trainset.X
     dataloader = torch.utils.data.DataLoader(trainset,batch_size=1,shuffle=False,num_workers=6)
 
-    #with_mlp = True
-    with_static = False
     if N == 0:
         with_mlp = False
-        with_cabac = False
-        with_static = True
 
     if with_mlp:
 
@@ -218,15 +208,14 @@ def backward_adaptive_coding(pths,N,lr,with_cabac=False,with_mlp=True):
         
         mlp_running_loss = 0.0
     
-    if with_cabac:
-        cabac = RealTimeCABAC(N)
-        cabac_avg_code_length_history = []
-        cabac_running_loss = 0.0
-
-    if with_static:
-        static = RealTimeAC()
-        static_avg_code_length_history = []
-        static_running_loss = 0.0
+    if with_lut:
+        lut_avg_code_length_histories = dict()
+        lut_running_losses = dict()
+        luts = dict()
+        for central_tendency in central_tendencies:
+            luts[central_tendency] = RealTimeLUT(N,central_tendency=central_tendency)
+            lut_avg_code_length_histories[central_tendency] = []
+            lut_running_losses[central_tendency] = 0.0 
 
     iteration = 0
 
@@ -251,23 +240,16 @@ def backward_adaptive_coding(pths,N,lr,with_cabac=False,with_mlp=True):
         assert y_b.item() == y[iteration,0]
         assert np.allclose(X_b.cpu().numpy().reshape(-1) , X[iteration,:].reshape(-1))
 
-        if with_cabac:
-            cabac_pred_t = cabac.predict(X[iteration:iteration+1,:])
-            cabac.update(X[iteration:iteration+1,:],y[iteration:iteration+1,:])
+        if with_lut:
+            for central_tendency in central_tendencies:
 
-            cabac_loss = perfect_AC(y[iteration:iteration+1,:],cabac_pred_t)
+                lut_pred_t = luts[central_tendency].predict(X[iteration:iteration+1,:])
+                luts[central_tendency].update(X[iteration:iteration+1,:],y[iteration:iteration+1,:])
 
-            cabac_running_loss += cabac_loss
-            cabac_avg_code_length_history.append( cabac_running_loss / (iteration + 1) )
-        
-        if with_static:
-            static_pred_t = static.predict()
-            static.update(y[iteration:iteration+1,:])
+                lut_loss = perfect_AC(y[iteration:iteration+1,:],lut_pred_t)
 
-            static_loss = perfect_AC(y[iteration:iteration+1,:],static_pred_t)
-
-            static_running_loss += static_loss
-            static_avg_code_length_history.append( static_running_loss / (iteration + 1) )
+                lut_running_losses[central_tendency] += lut_loss
+                lut_avg_code_length_histories[central_tendency].append( lut_running_losses[central_tendency] / (iteration + 1) )
 
 
         iteration += 1
@@ -276,15 +258,14 @@ def backward_adaptive_coding(pths,N,lr,with_cabac=False,with_mlp=True):
     data = dict()
     if with_mlp:
         data["MLPlr={:.0e}".format(lr)] = mlp_avg_code_length_history
-    if with_cabac:
-        data["LUT"] = cabac_avg_code_length_history
-    if with_static:
-        data["STATIC"] = static_avg_code_length_history
-    
+    if with_lut:
+        for central_tendency in central_tendencies:
+            data[f"LUT{central_tendency}"] = lut_avg_code_length_histories[central_tendency]
+
     return data
 
 
-def backward_adaptive_coding_experiment(exp_name,docs,Ns,learning_rates):
+def backward_adaptive_coding_experiment(exp_name,docs,Ns,learning_rates,central_tendencies,colors,linestyles):
 
     max_N = 26
 
@@ -296,23 +277,25 @@ def backward_adaptive_coding_experiment(exp_name,docs,Ns,learning_rates):
         if N > 0:
             for lr in learning_rates:
                 data["MLPlr={:.0e}".format(lr)] = np.zeros((len_data))
-            if (N<=max_N):
-                data["LUT"] = np.zeros((len_data))
-        else:
-            data["STATIC"] = np.zeros((len_data))
+        if (N<=max_N):
+            for central_tendency in central_tendencies:
+                data[f"LUT{central_tendency}"] = np.zeros((len_data))
+
 
         for doc in docs:
+            partial_data = dict()
             if N > 0:
                 for i_lr,lr in enumerate(learning_rates):
-                    partial_data = backward_adaptive_coding(doc,N,lr)
+                    with_lut = ((i_lr == len(learning_rates)-1) and (N<=max_N))
+                    partial_data = backward_adaptive_coding(doc,N,lr,central_tendencies,with_lut=with_lut)
                     k = "MLPlr={:.0e}".format(lr)
                     data[k] = data[k] + np.array(partial_data[k])
-                if (N<=max_N):
-                    partial_data = backward_adaptive_coding(doc,N,0,with_cabac=True,with_mlp=False)
-                    data["LUT"] = data["LUT"] + np.array(partial_data["LUT"])
-            else:
-                partial_data = backward_adaptive_coding(doc,0,0)
-                data["STATIC"] = data["STATIC"] + np.array(partial_data["STATIC"])
+            if (N<=max_N):
+                if not all([f"LUT{central_tendency}" in partial_data.keys() for central_tendency in central_tendencies]):
+                    partial_data = backward_adaptive_coding(doc,N,0,central_tendencies,with_lut=True,with_mlp=False)
+                for central_tendency in central_tendencies:
+                    k = f"LUT{central_tendency}"
+                    data[k] = data[k] + np.array(partial_data[k])
 
         for k in data.keys():
             data[k] = data[k]/len(docs)
@@ -320,14 +303,17 @@ def backward_adaptive_coding_experiment(exp_name,docs,Ns,learning_rates):
         xvalues = np.arange( len_data )
             
         fig = plot_comparison(xvalues,data,"iteration",
-            linestyles={k:"solid" for k in data.keys()},
-            colors={k:c for k,c in zip(data.keys(),["g","b","r","c","m","y","k","0.75"])},
+            linestyles={k:ls for k,ls in zip(data.keys(),linestyles)},
+            colors={k:c for k,c in zip(data.keys(),colors)},
             markers={k:"" for k in data.keys()})
 
         xticks = np.round(np.linspace(0,len_data-1,5)).astype(int)
 
         fig.axes[0].set_xticks( xticks)
         fig.axes[0].set_xticklabels( xticks)
+
+        ax, = fig.axes
+        ax.set_ylim([0.0, 0.5])
 
         fname = f"backward_adaptive_coding_{exp_name}_N{N}"
 
@@ -342,7 +328,7 @@ if __name__ == "__main__":
 
     # exp_name = "SPL2021_last_10_sorted_pages_corrected_lut"
 
-    exp_name = "Adaptive_Detection_of_Dim_corrected_lut_lr1e-1_lr1e-2_lr1e-4"
+    exp_name = "Adaptive_Detection_of_Dim_lut_mean_mode"
 
     docs = [ # docs[i,j] = the path to the j'th page from the i'th document
         [
@@ -356,8 +342,32 @@ if __name__ == "__main__":
 
     # docs = [[os.path.join('SPL2021',f)] for f in sorted(os.listdir('SPL2021'))[-10:]]
 
-    Ns = [10] # [0,2,4,10,26,67,170] # [26,33,42,53,67,84,107,135,170]
+    Ns = [26] # [0,2,4,10,26,67,170] # [26,33,42,53,67,84,107,135,170]
     
-    learning_rates = (3.162277659**np.array([-2,-4,-8]))
+    learning_rates = [] #(3.162277659**np.array([-2,-4,-8]))
 
-    backward_adaptive_coding_experiment(exp_name,docs,Ns,learning_rates)
+    central_tendencies = ["mode","mean"]
+
+    linestyles = [
+        "solid", # "LUTmode"
+        # "dashdot",
+        # "dashed",
+        # "dotted",
+        # linestyle_tuple['loosely dashdot'], 
+        # linestyle_tuple['loosely dashed'],
+        # linestyle_tuple['loosely dotted'],
+        linestyle_tuple['dashdotdotted'] # "LUTmean"
+    ]
+
+    colors = [
+        "g",
+        # "r",
+        # "b",
+        # "c",
+        # "m",
+        # "y",
+        # "k",
+        "0.5"
+    ]
+
+    backward_adaptive_coding_experiment(exp_name,docs,Ns,learning_rates,central_tendencies,colors,linestyles)
