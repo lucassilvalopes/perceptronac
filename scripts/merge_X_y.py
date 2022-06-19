e3ee974cafc46dbf7c3b104aabd6883db85cf78a
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2022-06-19 11:19:32 -0300

    updating the script to merge X and y data

--- scripts/merge_X_y.py
+++ scripts/merge_X_y.py
@@ -1,13 +1,8 @@
 import numpy as np
 import sys
+from tqdm import tqdm
 
-if __name__ == "__main__":
-
-    X_path = sys.argv[1]
-
-    y_path = sys.argv[2]
-
-    Xy_path = sys.argv[3]
+def merge_X_y(X_path,y_path,Xy_path):
 
     X = np.genfromtxt(X_path)
 
@@ -18,3 +13,24 @@ if __name__ == "__main__":
     Xy = np.concatenate([X,y],axis=1)
 
     np.savetxt(Xy_path, Xy, fmt="%d")
+
+
+if __name__ == "__main__":
+
+    X_path = "/home/lucas/Documents/data/eduardo/{}_frame000{}_contexts.txt"
+    
+    y_path = "/home/lucas/Documents/data/eduardo/{}_frame000{}_symbols.txt"
+    
+    Xy_path = "/home/lucas/Documents/data/eduardo/{}_frame000{}_contexts_symbols.txt"
+
+
+    people = ["andrew","david","phil","ricardo","sarah"]
+
+    frames = list(range(0,10))
+
+    pbar = tqdm(total=len(people)*len(frames))
+    for person in people:
+        for frame in frames:
+            merge_X_y(X_path.format(person,frame),y_path.format(person,frame),Xy_path.format(person,frame))
+            pbar.update(1)
+    pbar.close()
