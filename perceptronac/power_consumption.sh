b53e2009a5a3e812e4367b33a125ed617ffbcb3f
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2022-07-20 00:21:44 -0300

    shell script to monitor power consumption

---
+++ perceptronac/power_consumption.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+while true
+do 
+
+    start_epoch=$(date +%s)
+    target_epoch=$(( $start_epoch + 1 ))
+
+    TIME=$(date +%s.%N)
+    POWER=$(nvidia-smi -q -d POWER | grep -P '[\d\.]+ (?=W)' --o)
+    if [[ $POWER ]]
+    then
+        :
+    else
+        POWER=$(echo "00.00")
+    fi
+    echo "${TIME} ${POWER}" >> test.txt
+
+    current_epoch=$(date +%s)
+
+    if [ "$current_epoch" -lt "$target_epoch" ]
+    then
+        sleep_seconds=$(( $target_epoch - $current_epoch ))
+    else
+        sleep_seconds=$(( 0 ))
+    fi
+
+    sleep $sleep_seconds
+done 
