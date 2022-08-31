8879b5eecee479513b627d48cee18104053808f3
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2022-08-31 18:47:03 -0300

    rnn coding 1 page experiment

--- scripts/rnn_online_coding_grph_1page.py
+++ scripts/rnn_online_coding_grph_1page.py
@@ -6,15 +6,15 @@ if __name__ == "__main__":
 
     lr = 0.001
 
-    exp_name = f"Adaptive_Detection_of_Dim_5pages_lut_mean_lr1e{str(int(np.log10(lr)))}"
+    exp_name = f"Adaptive_Detection_of_Dim_page1_lr1e{str(int(np.log10(lr)))}"
 
     docs = [ # docs[i,j] = the path to the j'th page from the i'th document
         [
             "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_1.png",
-            "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_2.png",
-            "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_3.png",
-            "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_4.png",
-            "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_5.png",
+            # "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_2.png",
+            # "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_3.png",
+            # "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_4.png",
+            # "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_5.png",
         ]
     ]
 
