fabea95bae957c059245838187721b7139d231e1
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2022-12-27 21:08:08 -0300

    table N0-170 experiment (test portion)

--- scripts/fwrd_cdng_table_N0-170_test.py
+++ scripts/fwrd_cdng_table_N0-170_test.py
@@ -15,18 +15,18 @@ if __name__ == "__main__":
             os.path.join('SPL2020',f) for f in os.listdir('SPL2020')
         ],
         "validation_set": [
-            os.path.join('SPL2020V',f) for f in sorted(os.listdir('SPL2020V'))[:5] # validation
-            # os.path.join('SPL2021',f) for f in sorted(os.listdir('SPL2021'))[-10:] # test
+            # os.path.join('SPL2020V',f) for f in sorted(os.listdir('SPL2020V'))[:5] # validation
+            os.path.join('SPL2021',f) for f in sorted(os.listdir('SPL2021'))[-10:] # test
         ],
-        "epochs": 300,
+        "epochs": 1, # 300,
         "learning_rate": 0.00001,
         "batch_size": 2048,
         "num_workers":4,
         "device":"cuda:0", #"cpu"
-        "parent_id": "1671837729",
-        "unfinished_training": 1, # 0 or 1
+        "parent_id": "1672069178",
+        "unfinished_training": 0, # 0 or 1
         "N_vec": sorted([0] + [round(pow(1.595, i)) for i in range(12) if (i+1)%2==0],reverse=False),
-        "phases": ['train', 'valid'], # ['coding'],
+        "phases": ['valid'], #['train', 'valid'], # ['coding'],
         "xscale": 'symlog',
         "reduction": 'min', # min, last
         "data_type": 'image', # image, pointcloud
