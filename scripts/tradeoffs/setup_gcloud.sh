f9ba420a76f4fd22aac1231b0b1f072e9e4518f7
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2023-08-29 12:40:50 -0300

    update setup_gcloud.sh

--- scripts/tradeoffs/setup_gcloud.sh
+++ scripts/tradeoffs/setup_gcloud.sh
@@ -2,12 +2,10 @@
 
 curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
 sudo python3 install_gpu_driver.py
-cd ~
 sudo apt -y install python3-pip
 sudo apt -y install python3-virtualenv
 virtualenv compressai
 source compressai/bin/activate
 pip install pandas
 pip install compressai==1.2.4
-cd ~
 mkdir results_vae
