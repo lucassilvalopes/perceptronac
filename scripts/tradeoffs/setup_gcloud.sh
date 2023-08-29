cd681055508d1c9503719dc2b744fb981fe2926e
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2023-08-29 12:50:33 -0300

    update setup_gcloud.sh

--- scripts/tradeoffs/setup_gcloud.sh
+++ scripts/tradeoffs/setup_gcloud.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd ~
 curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
 sudo python3 install_gpu_driver.py
 sudo apt -y install python3-pip
