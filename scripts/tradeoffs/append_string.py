90794d979f1d5861e25480dafb49758fdc14fd67
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2023-09-30 09:31:43 -0300

    better append_strings.py

--- scripts/tradeoffs/append_string.py
+++ scripts/tradeoffs/append_string.py
@@ -3,9 +3,13 @@ import sys
 
 if __name__ == "__main__":
 
-    fldr = sys.argv[1]
-    strng = sys.argv[2] # "L5e-3_"
+    strng = sys.argv[1] # "L5e-3_"
 
-    for f in os.listdir(fldr):
-        new_f = f"{strng}{f}"
-        os.rename(os.path.join(fldr,f), os.path.join(fldr,new_f))
+    fldrs = []
+    for i in range(2,len(sys.argv)):
+        fldrs.append(sys.argv[i])
+
+    for fldr in fldrs:
+        for f in os.listdir(fldr):
+            new_f = f"{strng}{f}"
+            os.rename(os.path.join(fldr,f), os.path.join(fldr,new_f))
