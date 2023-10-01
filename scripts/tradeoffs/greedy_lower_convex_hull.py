65baaca58d4a1ca6e1b391391f0630ea0f126634
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2023-10-01 00:26:07 -0300

    refactoring

--- scripts/tradeoffs/greedy_lower_convex_hull.py
+++ scripts/tradeoffs/greedy_lower_convex_hull.py
@@ -1,33 +1,11 @@
-# %%
-import pandas as pd
-import sys
 import os
-import re
-
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(os.path.dirname(SCRIPT_DIR)))
-
-from perceptronac.convex_hull import convex_hull
+import pandas as pd
+import numpy as np
 from perceptronac.power_consumption import estimate_joules, get_n_pixels
 from perceptronac.power_consumption import group_energy_measurements
-import numpy as np
-import math
-import matplotlib
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
-from glch_utils import min_max_convex_hull
-
 from glch_BandD import GLCH
-
-
-
-matplotlib.use("pgf")
-matplotlib.rcParams.update({
-    "pgf.texsystem": "pdflatex",
-    "legend.fontsize": 14,
-    "xtick.labelsize": 14,
-    "ytick.labelsize": 14,
-    "pgf.preamble": [r"\usepackage{siunitx}"]})
+from decimal import Decimal
+from glch_utils import save_tree_data, save_hull_data, save_trees_data, save_hulls_data
 
 
 
@@ -51,267 +29,24 @@ def build_gho_tree(
     ).build_tree()
 
 
-# %%
-def print_tree(node,file=None):
-    # print("parent", node)
-
-    if len(node.children) == 0:
-        return
-
-    children_str = ""
-    for i,c in enumerate(node.children):
-        if i in node.chosen_child_indices:
-            prefix = ''.join(["!"] * (node.chosen_child_indices.index(i)+1) )
-        else:
-            prefix = ""
-        
-        children_str += f"{prefix}{c} "
-
-    print(node,children_str,file=file)
-
-    print("\n",file=file)
-    
-    for i,c in enumerate(node.children):
-        print_tree(node.children[i],file=file)
-
-
-# %%
-
-from line_clipping import cohenSutherlandClip
-
-
-def paint_tree(ax,data,node,x_axis,y_axis,x_range,y_range):
-    """
-    x_axis = "joules"
-    y_axis = "data_bits/data_samples"
-
-    https://stackoverflow.com/questions/34017866/arrow-on-a-line-plot-with-matplotlib
-    """
-
-    if len(node.children) == 0:
-        return
-
-    for i,c in enumerate(node.children):
-        if i in node.chosen_child_indices:
-            if node.chosen_child_indices.index(i) == 0:
-                color = "green"
-            else:
-                color = "darkgoldenrod"
-        else:
-            color = "firebrick"
-        line_x_vec = data.loc[[str(node), str(c)],x_axis].values
-        line_y_vec = data.loc[[str(node), str(c)],y_axis].values
-
-        if (x_range is not None) and (y_range is not None):
-
-            xd = 0.01 * np.abs(x_range[1] - x_range[0])
-            yd = 0.01 * np.abs(y_range[1] - y_range[0])
-
-            x1,y1,x2,y2 = cohenSutherlandClip(
-                x_range[0]+xd,x_range[1]-xd,y_range[0]+yd,y_range[1]-yd,line_x_vec[0],line_y_vec[0],line_x_vec[1],line_y_vec[1])
-        else:
-            x1,y1,x2,y2 = line_x_vec[0], line_y_vec[0], line_x_vec[1], line_y_vec[1]
-
-        if (x1 is not None) and (y1 is not None) and (x2 is not None) and (y2 is not None):
-            ax.annotate('',
-                xytext= (x1,y1),
-                xy= (x2,y2),
-                arrowprops=dict(arrowstyle="->", color=color),
-                # size=size
-            )
-    
-    for i,c in enumerate(node.children):
-        paint_tree(ax,data,node.children[i],x_axis,y_axis,x_range,y_range)
-
-# %%
-
-def tree_nodes(n, points, mode):
-
-    for i,c in enumerate(n.children):
-        if mode == "all":
-            points.append(str(c))
-        elif mode == "first":
-            if (len(n.chosen_child_indices) > 0) and (i == n.chosen_child_indices[0]):
-                points.append(str(c))
-        elif mode == "second":
-            if (i in n.chosen_child_indices) and (i != n.chosen_child_indices[0]):
-                points.append(str(c))
-        elif mode == "lch":
-            if c.lch:
-                points.append(str(c))
-        else:
-            raise ValueError(mode)
-
-    for i,c in enumerate(n.children):
-        tree_nodes(n.children[i],points,mode)
-
-    return points
-
-
-
-
-# %%
-
-
-def paint_cloud(data,x_axis,y_axis,ax,marker):
-    ax.plot(data.loc[:,x_axis].values,data.loc[:,y_axis].values,linestyle="",color="tab:blue",marker=marker)
-
-
-def paint_root(data,r,x_axis,y_axis,ax):
-    ax.plot([data.loc[str(r),x_axis]],[data.loc[str(r),y_axis]],linestyle="",color="yellow",marker="o")
-
-
-def adjust_axes(x_axis,y_axis,x_range,y_range,ax,x_in_log_scale=False,x_alias=None,y_alias=None):
-    """
-    x_axis = "joules"
-    y_axis = "data_bits/data_samples"
-    """
-    
-    if x_in_log_scale:
-        ax.set_xscale("log")
-        # xvalues = ax.get_xticks()
-        # ub = np.min(xvalues[xvalues>=np.max(data[x_col])])
-        # lb = np.max(xvalues[xvalues<=np.min(data[x_col])])
-        # xvalues = xvalues[np.logical_and(xvalues>=lb,xvalues<=ub)]
-        # ax.set_xticklabels([])
-        # ax.set_xticklabels([], minor=True)
-        # ax.set_xticks(xvalues)
-        # ax.set_xticklabels(xvalues)
-
-    ax.set_xlabel(x_alias if x_alias else x_axis, fontsize=16)
-    ax.set_ylabel(y_alias if y_alias else y_axis, fontsize=16)
-    if x_range:
-        ax.set_xlim(x_range[0],x_range[1])
-    if y_range:
-        ax.set_ylim(y_range[0],y_range[1])
-
-
-# %%
-
-def compute_hulls(data,rs,x_axis,y_axis):
-
-    true_hull_points = data.iloc[min_max_convex_hull(data.loc[:,[x_axis,y_axis]].values.tolist()),:]
-    
-    new_points = []
-    for r in rs:
-        new_points += [str(r)]
-        new_points += tree_nodes(r,[],"all")
-    probe = data.loc[new_points,:]
-    estimated_hull_points = probe.iloc[min_max_convex_hull(probe.loc[:,[x_axis,y_axis]].values.tolist()),:]
-
-    return true_hull_points,estimated_hull_points
-
-
-def get_n_trained_networks(rs):
-    new_points = []
-    for r in rs:
-        new_points += [str(r)]
-        new_points += tree_nodes(r,[],"all")
-    n_trained_networks = len(set([re.sub(r'_[\d]{2}b','',pt) for pt in new_points]))
-    return n_trained_networks
-
-
-def paint_hulls(true_hull_points,estimated_hull_points,x_axis,y_axis,ax):
-    """
-    x_axis = "joules"
-    y_axis = "data_bits/data_samples"
-    """
-    ax.plot(true_hull_points[x_axis],true_hull_points[y_axis],linestyle=(0, (5, 5)),color="orangered",marker=None)
-    ax.plot(
-        estimated_hull_points[x_axis],estimated_hull_points[y_axis],linestyle="dotted",color="black",marker=None)
-
-
-def paint_hull_points(true_hull_points,x_axis,y_axis,ax):
-
-    ax.plot(true_hull_points[x_axis],true_hull_points[y_axis],linestyle="",color="orangered",marker=".")
-
-
-def paint_hull(true_hull_points,estimated_hull_points,x_axis,y_axis,ax):
-    """
-    x_axis = "joules"
-    y_axis = "data_bits/data_samples"
-    """
-    ax.plot(
-        estimated_hull_points[x_axis],estimated_hull_points[y_axis],linestyle="",color="black",marker="o",
-        markerfacecolor='none',markersize=8)
-    # plt.scatter(
-    #     estimated_hull_points[x_axis],estimated_hull_points[y_axis],facecolors='none', edgecolors='k')
-
-
-
-
-def paint_hull_nodes(data,r,x_axis,y_axis,ax):
-
-    lch_nodes = [str(r)] + tree_nodes(r,[], "lch")
-    lch_nodes_xy = data.loc[lch_nodes,:]
-    ax.plot(lch_nodes_xy[x_axis],lch_nodes_xy[y_axis],linestyle="",color="black",marker="o",
-            markerfacecolor='none',markersize=8)
-
-
-def paint_tree_nodes(data,r,x_axis,y_axis,ax):
-
-    first_selected_nodes = [str(r)] + tree_nodes(r,[], "first")
-    second_selected_nodes = tree_nodes(r,[], "second")
-    first_selected_nodes_xy = data.loc[first_selected_nodes,:]
-    second_selected_nodes_xy = data.loc[second_selected_nodes,:]
-    all_nodes = [str(r)] + tree_nodes(r,[], "all")
-    unselected_nodes = list((set(all_nodes) - set(first_selected_nodes)) - set(second_selected_nodes) )
-    unselected_nodes_xy = data.loc[unselected_nodes,:]
-    ax.plot(first_selected_nodes_xy[x_axis],first_selected_nodes_xy[y_axis],linestyle="",color="green",marker=".")
-    ax.plot(second_selected_nodes_xy[x_axis],second_selected_nodes_xy[y_axis],linestyle="",color="darkgoldenrod",marker=".")
-    ax.plot(unselected_nodes_xy[x_axis],unselected_nodes_xy[y_axis],linestyle="",color="firebrick",marker=".")
-
-
-def save_tree_data(data,r,x_axis,y_axis,x_range,y_range,data_id,x_in_log_scale=False,x_alias=None,y_alias=None,fldr="glch_results"):
-
-    n_trained_networks = get_n_trained_networks([r])
-
-    with open(f'{fldr}/tree_{data_id}.txt', 'w') as f:
-        print_tree(r,file=f)
-        print(f"number of trained networks : {n_trained_networks}",file=f)
-
-    tree_fig, ax = plt.subplots(nrows=1, ncols=1)
-    paint_cloud(data,x_axis,y_axis,ax,".")
-    paint_root(data,r,x_axis,y_axis,ax)
-    paint_tree(ax,data,r,x_axis,y_axis,x_range,y_range)
-    # paint_hull_points(true_hull_points,x_axis,y_axis,ax)
-    paint_tree_nodes(data,r,x_axis,y_axis,ax)
-    adjust_axes(x_axis,y_axis,x_range,y_range,ax,x_in_log_scale,x_alias,y_alias)
-    tree_fig.savefig(f"{fldr}/tree_fig_{data_id}.png", dpi=300, facecolor='w', bbox_inches = "tight")
-
-
-def save_hull_data(data,r,x_axis,y_axis,x_range,y_range,data_id,x_in_log_scale=False,x_alias=None,y_alias=None,fldr="glch_results"):
-
-    true_hull_points,estimated_hull_points = compute_hulls(data,[r],x_axis,y_axis)
-
-    hulls_fig, ax = plt.subplots(nrows=1, ncols=1)
-    paint_cloud(data,x_axis,y_axis,ax,"x")
-    # paint_hulls(true_hull_points,estimated_hull_points,x_axis,y_axis,ax)
-    # paint_hull(true_hull_points,estimated_hull_points,x_axis,y_axis,ax)
-    paint_hull_nodes(data,r,x_axis,y_axis,ax)
-    adjust_axes(x_axis,y_axis,None,None,ax,x_in_log_scale,x_alias,y_alias)
-    hulls_fig.savefig(f"{fldr}/hulls_fig_{data_id}.png", dpi=300, facecolor='w', bbox_inches = "tight")
-
-    save_hull_points(f"{fldr}/hulls_{data_id}",true_hull_points,estimated_hull_points)
-
-
-# %%
-from decimal import Decimal
-
-# https://stackoverflow.com/questions/45332056/decompose-a-float-into-mantissa-and-exponent-in-base-10-without-strings
-
 def fexp(number):
     (sign, digits, exponent) = Decimal(number).as_tuple()
     return len(digits) + exponent - 1
 
+
 def fman(number):
     return Decimal(number).scaleb(-fexp(number)).normalize()
+
 
 def limit_significant_digits(value,last_significant_digit_position):
     factor = 10**last_significant_digit_position
     return np.round(value/factor) * factor
 
+
 def limit_energy_significant_digits(data,x_axis):
+    """
+    https://stackoverflow.com/questions/45332056/decompose-a-float-into-mantissa-and-exponent-in-base-10-without-strings
+    """
 
     mean_col=x_axis
     std_col=f"{x_axis}_std"
@@ -325,17 +60,6 @@ def limit_energy_significant_digits(data,x_axis):
         # std_col:limit_significant_digits(x[std_col],fexp(x[std_col]))
     },index=[mean_col,std_col]), axis=1)
     return data
-
-# %%
-
-
-def save_hull_points(file_name,true_hull_points,estimated_hull_points):
-
-    with open(f'{file_name}.txt', 'w') as f:
-        print("true_hull_points",file=f)
-        print(true_hull_points,file=f)
-        print("estimated_hull_points",file=f)
-        print(estimated_hull_points,file=f)
 
 
 def get_energy_data(csv_path,remove_noise):
@@ -424,7 +148,6 @@ def glch_rate_vs_params(*args,**kwargs):
     glch_rate_vs_energy(*args,**kwargs)
 
 
-
 def glch_rate_vs_dist(
         csv_path,x_axis,y_axis,
         x_range=None,y_range=None,
@@ -489,14 +212,6 @@ def glch_rate_vs_dist(
             x_alias=x_alias,y_alias=y_alias,fldr=fldr)
 
 
-def get_x_range_y_range(data,x_axis,y_axis):
-
-    tmp_fig, tmp_ax = plt.subplots(nrows=1, ncols=1)
-    paint_cloud(data,x_axis,y_axis,tmp_ax,".")
-    x_range,y_range = tmp_ax.get_xlim(),tmp_ax.get_ylim()
-    return x_range,y_range
-
-
 def glch_rate_vs_dist_2(
     csv_path,x_axis,y_axis,x_range=None,y_range=None,start="left",constrained=True,fldr="glch_results"):
     """only for glch algo"""
@@ -533,14 +248,6 @@ def glch_rate_vs_dist_2(
     exp_id = f"{x_axis}_vs_{y_axis}_brute_{brute_keys}_greedy_{greedy_keys}_start_{start}"
 
     rs = []
-    tree_file = open(f'{fldr}/tree_{exp_id}.txt', 'w')
-
-    tree_fig, ax = plt.subplots(nrows=1, ncols=len(brute_dict["L"]))
-
-
-    if (x_range is None) and (y_range is None):
-        x_range,y_range = get_x_range_y_range(data,x_axis,y_axis)
-
     for i,L in enumerate(brute_dict["L"]):
 
         to_str_method = to_str_method_factory({"L":L})
@@ -555,40 +262,8 @@ def glch_rate_vs_dist_2(
         
         rs.append(r)
 
-        print_tree(r,file=tree_file)
-
-        paint_root(data,r,x_axis,y_axis,tree_fig.axes[i])
-        # paint_cloud(data,x_axis,y_axis,tree_fig.axes[i],".")
-        paint_cloud(current_data,x_axis,y_axis,tree_fig.axes[i],".")
-        paint_tree(tree_fig.axes[i],data,r,x_axis,y_axis,x_range,y_range)
-        adjust_axes(x_axis,y_axis,x_range,y_range,tree_fig.axes[i])
-
-        if i != 0:
-            tree_fig.axes[i].set_yticks([])
-            tree_fig.axes[i].set_ylabel('')
-    
-    true_hull_points,estimated_hull_points,n_trained_networks = compute_hulls(data,rs,x_axis,y_axis)
-
-    print(f"number of trained networks : {n_trained_networks}",file=tree_file)
-    tree_file.close()
-
-    for i in range(len(brute_dict["L"])):
-        # paint_hull_points(true_hull_points,x_axis,y_axis,tree_fig.axes[i])
-        paint_tree_nodes(data,rs[i],x_axis,y_axis,tree_fig.axes[i])
-
-    tree_fig.savefig(f"{fldr}/tree_fig_{exp_id}.png", dpi=300, facecolor='w', bbox_inches = "tight")
-
-    
-    hulls_fig, ax = plt.subplots(nrows=1, ncols=1)
-    paint_cloud(data,x_axis,y_axis,ax,"x")
-    # paint_hulls(true_hull_points,estimated_hull_points,x_axis,y_axis,ax)
-    # paint_hull(true_hull_points,estimated_hull_points,x_axis,y_axis,ax)
-    for i in range(len(brute_dict["L"])):
-        paint_hull_nodes(data,rs[i],x_axis,y_axis,ax)
-    adjust_axes(x_axis,y_axis,None,None,ax)
-    hulls_fig.savefig(f"{fldr}/hulls_fig_{exp_id}.png", dpi=300, facecolor='w', bbox_inches = "tight")
-
-    save_hull_points(f"{fldr}/hulls_fig_{exp_id}",true_hull_points,estimated_hull_points)
+    save_trees_data(data,rs,brute_dict["L"],x_axis,y_axis,x_range,y_range,exp_id,fldr=fldr)
+    save_hulls_data(data,rs,brute_dict["L"],x_axis,y_axis,x_range,y_range,exp_id,fldr=fldr)
 
 
 def glch_model_bits_vs_data_bits(
