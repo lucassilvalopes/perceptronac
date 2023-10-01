516e53cdec464529b733a594b9cc5b1a6f6f1590
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2023-10-01 01:08:49 -0300

    first greedy hyperparameter optimization example

---
+++ scripts/tradeoffs/greedy_hyperparameter_optimization.py
@@ -0,0 +1,32 @@
+import os
+from glch_functions import glch_rate_vs_dist
+
+
+if __name__ == "__main__":
+
+    results_folder = "gho_results"
+    debug_folder = "gho_debug"
+
+    if os.path.isdir(results_folder):
+        import shutil
+        shutil.rmtree(results_folder)
+    os.mkdir(results_folder)
+
+    if os.path.isdir(debug_folder):
+        import shutil
+        shutil.rmtree(debug_folder)
+    os.mkdir(debug_folder)
+
+
+    glch_rate_vs_dist(
+        "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
+        "bpp_loss","mse_loss",
+        # x_range=[0.1,1.75],
+        # y_range=[0.001,0.0045]
+        lambdas=["2e-2"],
+        algo="gho",
+        constrained=True,
+        lmbda=2e-2*(255**2),
+        fldr=results_folder,
+        debug_folder=debug_folder
+    )
