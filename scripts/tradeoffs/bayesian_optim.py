1e97655dde61dfd826102964fa8e5a563417ca74
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2023-10-07 16:22:29 -0300

    rate-complexity-distortion bayes opt examples

--- scripts/tradeoffs/bayesian_optim.py
+++ scripts/tradeoffs/bayesian_optim.py
@@ -120,3 +120,17 @@ if __name__ == "__main__":
         [1,2e-2*(255**2)],
         lambdas=["2e-2"]
     )
+
+    bayes_opt_rate_dist(
+        "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
+        ["bpp_loss","mse_loss","params"],
+        [1,2e-2*(255**2),1/1e6],
+        lambdas=["2e-2"]
+    )
+
+    bayes_opt_rate_dist(
+        "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
+        ["bpp_loss","mse_loss","flops"],
+        [1,2e-2*(255**2),1/1e10],
+        lambdas=["2e-2"]
+    )
