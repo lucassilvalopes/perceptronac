d7279f558d62ef1fae55213a952670aac67f992b
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2023-10-08 10:29:22 -0300

    gho in multiple dimensions

--- scripts/tradeoffs/greedy_hyperparameter_optimization.py
+++ scripts/tradeoffs/greedy_hyperparameter_optimization.py
@@ -20,13 +20,30 @@ if __name__ == "__main__":
 
     glch_rate_vs_dist(
         "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
-        "bpp_loss","mse_loss",
-        # x_range=[0.1,1.75],
-        # y_range=[0.001,0.0045]
+        ["bpp_loss","mse_loss"],
+        # axes_ranges=[[0.1,1.75],[0.001,0.0045]]
         lambdas=["2e-2"],
         algo="gho",
         constrained=True,
-        lmbda=2e-2*(255**2),
+        weights=[1,2e-2*(255**2)],
         fldr=results_folder,
         debug_folder=debug_folder
+    )
+
+    glch_rate_vs_dist(
+        "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
+        ["bpp_loss","mse_loss","params"],
+        weights=[1,2e-2*(255**2),1/1e6],
+        lambdas=["2e-2"],
+        algo="gho",
+        constrained=True
+    )
+
+    glch_rate_vs_dist(
+        "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
+        ["bpp_loss","mse_loss","flops"],
+        weights=[1,2e-2*(255**2),1/1e10],
+        lambdas=["2e-2"],
+        algo="gho",
+        constrained=True
     )
