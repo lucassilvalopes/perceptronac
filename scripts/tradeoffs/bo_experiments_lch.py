9f4d0a0a0da261ed763c7cfc51698b4035c543b5
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2023-10-24 06:48:29 -0300

    fixing the lambda

--- scripts/tradeoffs/bo_experiments_lch.py
+++ scripts/tradeoffs/bo_experiments_lch.py
@@ -7,18 +7,18 @@ from bo_utils import simple_lambda_grid_3d,lambda_grid_3d,lambda_seq
 
 if __name__ == "__main__":
 
-    # bayes_lch_rate_dist(
-    #     "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
-    #     ["bpp_loss","mse_loss","params"],
-    #     simple_lambda_grid_3d(),
-    #     # lambda_grid_3d(y_lmbd=[0.005,0.01,0.02,500],z_lmbd=[1/1e+6]),
-    #     # ax_ranges=[[0,4],[0.0025,0.0125],[0,9*1e6]],
-    #     random_state=1,
-    #     init_points=2,
-    #     n_iter=47,
-    #     acquisition_func="pii",
-    #     lch_method="jointly"
-    # )
+    bayes_lch_rate_dist(
+        "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
+        ["bpp_loss","mse_loss","params"],
+        simple_lambda_grid_3d(),
+        lambdas=["2e-2"],
+        random_state=1,
+        init_points=2,
+        n_iter=47,
+        ax_ranges=None,
+        acquisition_func="pii",
+        lch_method="jointly"
+    )
 
     random_state = random.randint(0,100)
 
@@ -26,11 +26,11 @@ if __name__ == "__main__":
         "random",
         ["bpp_loss","mse_loss","params"],
         simple_lambda_grid_3d(),
-        # lambda_grid_3d(y_lmbd=[0.005,0.01,0.02,500],z_lmbd=[1/1e+6]),
-        # ax_ranges=[[0,4],[0.0025,0.0125],[0,9*1e6]],
+        lambdas=["2e-2"],
         random_state=random_state,
         init_points=2,
         n_iter=47,
+        ax_ranges=None,
         acquisition_func="random",
         lch_method="jointly"
     )
@@ -39,11 +39,11 @@ if __name__ == "__main__":
         "random",
         ["bpp_loss","mse_loss","params"],
         simple_lambda_grid_3d(),
-        # lambda_grid_3d(y_lmbd=[0.005,0.01,0.02,500],z_lmbd=[1/1e+6]),
-        # ax_ranges=[[0,4],[0.0025,0.0125],[0,9*1e6]],
+        lambdas=["2e-2"],
         random_state=random_state,
         init_points=2,
         n_iter=47,
+        ax_ranges=None,
         acquisition_func="pii",
         lch_method="jointly"
 
@@ -53,11 +53,11 @@ if __name__ == "__main__":
         "random",
         ["bpp_loss","mse_loss","params"],
         simple_lambda_grid_3d(),
-        # lambda_grid_3d(y_lmbd=[0.005,0.01,0.02,500],z_lmbd=[1/1e+6]),
-        # ax_ranges=[[0,4],[0.0025,0.0125],[0,9*1e6]],
+        lambdas=["2e-2"],
         random_state=random_state,
         init_points=2,
         n_iter=5,
+        ax_ranges=None,
         acquisition_func="pi",
         lch_method="repeatedly"
     )
