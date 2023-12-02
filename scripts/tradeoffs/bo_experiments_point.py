76c7754a983e0e56a33e3fb480ebdc79cf4d2b32
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2023-12-02 17:44:11 -0300

    reusable code

--- scripts/tradeoffs/bo_experiments_point.py
+++ scripts/tradeoffs/bo_experiments_point.py
@@ -1,7 +1,8 @@
 import random
 from bo_experiments_functions import bayes_opt_rate_dist
 
-if __name__ == "__main__":
+
+def bo_statistics(*args,**kwargs):
 
     original_random_state = random.getstate()
     random.seed(42)
@@ -13,13 +14,9 @@ if __name__ == "__main__":
     results_list = []
     for random_state in random_states:
 
-        lbl,n_trained_networks,target = bayes_opt_rate_dist(
-            "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
-            ["bpp_loss","mse_loss"],
-            [1,2e-2*(255**2)],
-            lambdas=["2e-2"],
-            random_state=random_state
-        )
+        kwargs["random_state"] = random_state
+
+        lbl,n_trained_networks,target = bayes_opt_rate_dist(*args,**kwargs)
         results_list.append((lbl,n_trained_networks,target))
     
     n_hits = sum([(1 if r[0] == "D3L2e-2N160M32" else 0) for r in results_list])
@@ -35,14 +32,25 @@ if __name__ == "__main__":
     print(f"best target: {best_target}")
     print(f"target higher on average by (%): {percent_higher}")
 
-    # bayes_opt_rate_dist(
+
+
+if __name__ == "__main__":
+
+    bo_statistics(
+        "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
+        ["bpp_loss","mse_loss"],
+        [1,2e-2*(255**2)],
+        lambdas=["2e-2"]
+    )
+
+    # bo_statistics(
     #     "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
     #     ["bpp_loss","mse_loss","params"],
     #     [1,2e-2*(255**2),1/1e6],
     #     lambdas=["2e-2"]
     # )
 
-    # bayes_opt_rate_dist(
+    # bo_statistics(
     #     "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
     #     ["bpp_loss","mse_loss","flops"],
     #     [1,2e-2*(255**2),1/1e10],
