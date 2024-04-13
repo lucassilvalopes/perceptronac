4bc04f20c374b9017ede56de5b789f7dfdc26919
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2024-04-13 10:02:46 -0300

    max hv

--- scripts/tradeoffs/ax_experiments_lch.py
+++ scripts/tradeoffs/ax_experiments_lch.py
@@ -28,6 +28,54 @@ from ax.runners.synthetic import SyntheticRunner
 from ax.service.utils.report_utils import exp_to_df
 from botorch.test_functions.multi_objective import BraninCurrin
 
+from ax_multiobjective_optimization import get_trials_hv
+
+
+def label_to_params(label):
+    splitm = label.split("M")
+    M = int(splitm[1])
+    splitn = splitm[0].split("N")
+    N = int(splitn[1])
+    splitl = splitn[0].split("L")
+    L = float(splitl[1])
+    splitd = splitl[0].split("D")
+    D = int(splitd[1])
+    return {"D":D,"L":L,"N":N,"M":M}
+
+
+def df_to_trials(df):
+    trials = []
+    for r,c in df.iterrows():
+        trials.append({
+            "input":label_to_params(r),
+            "output": {
+                "a":{"mean":c["bpp_loss"], "sem": 0},
+                "b":{"mean":c["mse_loss"], "sem": 0},
+                "c":{"mean":c["params"], "sem": 0}
+            }
+        })
+    return trials
+
+
+def get_glch_hv_list_rdc(search_space,optimization_config,glch_csv_name):
+
+    glch_csv = pd.read_csv(glch_csv_name)
+    glch_csv[["D","L","N","M"]] = glch_csv["labels"].apply(lambda x: pd.Series(label_to_params(x)))
+    glch_csv = glch_csv.sort_values(by=['iteration', 'D','N','M','L']).set_index("labels")
+
+    glch_hv_list = []
+
+    for i in range(glch_csv.shape[0]):
+
+        curr_data = glch_csv.iloc[:i+1,:]
+
+        curr_trials = df_to_trials(curr_data)
+        
+        curr_hv = get_trials_hv(search_space,optimization_config,curr_trials)
+        
+        glch_hv_list.append(curr_hv)
+    
+    return glch_hv_list
 
 
 def ax_rdc_setup(csv_path):
@@ -82,57 +130,11 @@ def ax_rdc_setup(csv_path):
         objective_thresholds=objective_thresholds,
     )
 
-    return search_space, optimization_config
+    max_hv_trials = df_to_trials(data)
 
+    max_hv = get_trials_hv(search_space,optimization_config,max_hv_trials)
 
+    return search_space, optimization_config, max_hv
 
-def label_to_params(label):
-    splitm = label.split("M")
-    M = int(splitm[1])
-    splitn = splitm[0].split("N")
-    N = int(splitn[1])
-    splitl = splitn[0].split("L")
-    L = float(splitl[1])
-    splitd = splitl[0].split("D")
-    D = int(splitd[1])
-    return {"D":D,"L":L,"N":N,"M":M}
-
-
-def df_to_trials(df):
-    trials = []
-    for r,c in df.iterrows():
-        trials.append({
-            "input":label_to_params(r),
-            "output": {
-                "a":{"mean":c["bpp_loss"], "sem": 0},
-                "b":{"mean":c["mse_loss"], "sem": 0},
-                "c":{"mean":c["params"], "sem": 0}
-            }
-        })
-    return trials
-
-
-from ax_multiobjective_optimization import get_trials_hv
-
-
-def get_glch_hv_list_rdc(search_space,optimization_config,glch_csv_name):
-
-    glch_csv = pd.read_csv(glch_csv_name)
-    glch_csv[["D","L","N","M"]] = glch_csv["labels"].apply(lambda x: pd.Series(label_to_params(x)))
-    glch_csv = glch_csv.sort_values(by=['iteration', 'D','N','M','L']).set_index("labels")
-
-    glch_hv_list = []
-
-    for i in range(glch_csv.shape[0]):
-
-        curr_data = glch_csv.iloc[:i+1,:]
-
-        curr_trials = df_to_trials(curr_data)
-        
-        curr_hv = get_trials_hv(search_space,optimization_config,curr_trials)
-        
-        glch_hv_list.append(curr_hv)
-    
-    return glch_hv_list
 
 
