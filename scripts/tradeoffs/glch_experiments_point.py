8b55bcfacf5798a7193012285e65ba32d43aba94
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2024-04-15 07:47:18 -0300

    point experiments

--- scripts/tradeoffs/glch_experiments_point.py
+++ scripts/tradeoffs/glch_experiments_point.py
@@ -21,77 +21,37 @@ if __name__ == "__main__":
     glch_rate_vs_dist(
         "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
         ["bpp_loss","mse_loss"],
-        # axes_ranges=[[0.1,1.75],[0.001,0.0045]]
         lambdas=["2e-2"],
         algo="gho",
         constrained=False,
         weights=[1,2e-2*(255**2)],
         fldr=results_folder,
-        debug_folder=debug_folder
+        debug_folder=debug_folder,
+        debug=True
     )
 
-    glch_rate_vs_dist(
-        "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
-        ["bpp_loss","mse_loss","params"],
-        weights=[1,5e-3*(255**2),1/(1e6 * 100)],
-        lambdas=["5e-3"],
-        algo="gho",
-        constrained=False,
-        fldr=results_folder,
-        debug_folder=debug_folder
-    )
+    for lmbda,mult in [("5e-3",1),("5e-3",100),("1e-2",1),("1e-2",100),("2e-2",1),("2e-2",100)]:
 
-    glch_rate_vs_dist(
-        "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
-        ["bpp_loss","mse_loss","flops"],
-        weights=[1,5e-3*(255**2),1/(1e10 * 100)],
-        lambdas=["5e-3"],
-        algo="gho",
-        constrained=False,
-        fldr=results_folder,
-        debug_folder=debug_folder
-    )
+        glch_rate_vs_dist(
+            "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
+            ["bpp_loss","mse_loss","params"],
+            weights=[1,float(lmbda)*(255**2),1/(1e6 * mult)],
+            lambdas=[lmbda],
+            algo="gho",
+            constrained=False,
+            fldr=results_folder,
+            debug_folder=debug_folder,
+            debug=False
+        )
 
-    glch_rate_vs_dist(
-        "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
-        ["bpp_loss","mse_loss","params"],
-        weights=[1,1e-2*(255**2),1/(1e6 * 100)],
-        lambdas=["1e-2"],
-        algo="gho",
-        constrained=False,
-        fldr=results_folder,
-        debug_folder=debug_folder
-    )
-
-    glch_rate_vs_dist(
-        "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
-        ["bpp_loss","mse_loss","flops"],
-        weights=[1,1e-2*(255**2),1/(1e10 * 100)],
-        lambdas=["1e-2"],
-        algo="gho",
-        constrained=False,
-        fldr=results_folder,
-        debug_folder=debug_folder
-    )
-
-    glch_rate_vs_dist(
-        "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
-        ["bpp_loss","mse_loss","params"],
-        weights=[1,2e-2*(255**2),1/(1e6 * 100)],
-        lambdas=["2e-2"],
-        algo="gho",
-        constrained=False,
-        fldr=results_folder,
-        debug_folder=debug_folder
-    )
-
-    glch_rate_vs_dist(
-        "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
-        ["bpp_loss","mse_loss","flops"],
-        weights=[1,2e-2*(255**2),1/(1e10 * 100)],
-        lambdas=["2e-2"],
-        algo="gho",
-        constrained=False,
-        fldr=results_folder,
-        debug_folder=debug_folder
-    )
+        glch_rate_vs_dist(
+            "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
+            ["bpp_loss","mse_loss","flops"],
+            weights=[1,float(lmbda)*(255**2),1/(1e10 * mult)],
+            lambdas=[lmbda],
+            algo="gho",
+            constrained=False,
+            fldr=results_folder,
+            debug_folder=debug_folder,
+            debug=False
+        )
