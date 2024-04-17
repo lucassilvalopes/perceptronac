1d5fc64365ce83aff91dbc5291785c2326f8b87b
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2024-04-17 19:03:49 -0300

    other glch ax comparisons

--- scripts/tradeoffs/ax_experiments_lch.py
+++ scripts/tradeoffs/ax_experiments_lch.py
@@ -7,10 +7,15 @@ from ax_experiments_functions import rc_setup,rc_read_glch_data,rc_label_to_para
 from ax_experiments_functions import rb_setup,rb_read_glch_data,rb_label_to_params
 
 
+N_SEEDS = 25
+SEEDS_RANGE = [1, 10000]
+N_INIT = 6
+
+
 if __name__ == "__main__":
 
     ax_glch_comparison_mohpo(
-        results_folder="ax_results_rdc_params",
+        results_folder="ax_results",
         data_csv_path = "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
         setup_func=partial(rdc_setup,complexity_axis="params"),
         glch_csv_paths = {
@@ -22,16 +27,32 @@ if __name__ == "__main__":
         },
         read_glch_data_func=rdc_read_glch_data,
         label_to_params_func=rdc_label_to_params,
-        n_seeds = 1,
-        seeds_range = [1234,1235],
-        n_init=6
+        n_seeds = N_SEEDS,
+        seeds_range = SEEDS_RANGE,
+        n_init=N_INIT
+    )
+
+    ax_glch_comparison_mohpo(
+        results_folder="ax_results",
+        data_csv_path = "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
+        setup_func=partial(rdc_setup,complexity_axis="flops"),
+        glch_csv_paths = {
+            "c_angle_rule": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_angle_rule_constrained_bpp_loss_vs_mse_loss_vs_params_start_left_threed_history.csv",
+            "u_angle_rule": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_angle_rule_unconstrained_bpp_loss_vs_mse_loss_vs_params_start_left_threed_history.csv",
+            "c_gift_wrapping": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_gift_wrapping_constrained_bpp_loss_vs_mse_loss_vs_params_start_left_threed_history.csv",
+            "u_gift_wrapping": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_gift_wrapping_unconstrained_bpp_loss_vs_mse_loss_vs_params_start_left_threed_history.csv",
+            "c_tie_break": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_tie_break_constrained_bpp_loss_vs_mse_loss_vs_params_start_left_threed_history.csv"
+        },
+        read_glch_data_func=rdc_read_glch_data,
+        label_to_params_func=rdc_label_to_params,
+        n_seeds = N_SEEDS,
+        seeds_range = SEEDS_RANGE,
+        n_init=N_INIT
     )
 
 
-
-
     ax_glch_comparison_mohpo(
-        results_folder="ax_results_rc_noisy_joules",
+        results_folder="ax_results",
         data_csv_path = "/home/lucas/Documents/perceptronac/scripts/tradeoffs/rate-noisy-joules-time-params_hx-10-20-40-80-160-320-640.csv",
         setup_func=partial(rc_setup,complexity_axis="micro_joules_per_pixel"),
         glch_csv_paths = {
@@ -43,16 +64,33 @@ if __name__ == "__main__":
         },
         read_glch_data_func=rc_read_glch_data,
         label_to_params_func=rc_label_to_params,
-        n_seeds = 1,
-        seeds_range = [1234,1235],
-        n_init=6
+        n_seeds = N_SEEDS,
+        seeds_range = SEEDS_RANGE,
+        n_init=N_INIT
     )
 
 
+    ax_glch_comparison_mohpo(
+        results_folder="ax_results",
+        data_csv_path = "/home/lucas/Documents/perceptronac/scripts/tradeoffs/rate-noisy-joules-time-params_hx-10-20-40-80-160-320-640.csv",
+        setup_func=partial(rc_setup,complexity_axis="params"),
+        glch_csv_paths = {
+            "c_angle_rule": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_angle_rule_constrained_rate_vs_energy_noisy_history.csv",
+            "u_angle_rule": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_angle_rule_unconstrained_rate_vs_energy_noisy_history.csv",
+            "c_gift_wrapping": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_gift_wrapping_constrained_rate_vs_energy_noisy_history.csv",
+            "u_gift_wrapping": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_gift_wrapping_unconstrained_rate_vs_energy_noisy_history.csv",
+            "c_tie_break": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_tie_break_constrained_rate_vs_energy_noisy_history.csv"
+        },
+        read_glch_data_func=rc_read_glch_data,
+        label_to_params_func=rc_label_to_params,
+        n_seeds = N_SEEDS,
+        seeds_range = SEEDS_RANGE,
+        n_init=N_INIT
+    )
 
 
     ax_glch_comparison_mohpo(
-        results_folder="ax_results_rc_model_bits",
+        results_folder="ax_results",
         data_csv_path = "/home/lucas/Documents/perceptronac/scripts/tradeoffs/rate-model-bits_hx-10-20-40-80-160-320-640_b-8-16-32.csv",
         setup_func=rb_setup,
         glch_csv_paths = {
@@ -64,7 +102,7 @@ if __name__ == "__main__":
         },
         read_glch_data_func=rb_read_glch_data,
         label_to_params_func=rb_label_to_params,
-        n_seeds = 1,
-        seeds_range = [1234,1235],
-        n_init=6
+        n_seeds = N_SEEDS,
+        seeds_range = SEEDS_RANGE,
+        n_init=N_INIT
     )
