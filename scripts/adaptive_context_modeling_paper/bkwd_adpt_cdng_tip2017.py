3f66dca4b277309729996f64a5136831c602820a
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2024-04-29 07:08:58 -0300

    updated apc tip2017 script

--- scripts/adaptive_context_modeling_paper/bkwd_adpt_cdng_tip2017.py
+++ scripts/adaptive_context_modeling_paper/bkwd_adpt_cdng_tip2017.py
@@ -5,48 +5,40 @@ from perceptronac.backward_adaptive_coding import backward_adaptive_coding_exper
 
 if __name__ == "__main__":
 
-    exp_name = "TIP2017_page1_lut_mean"
+    for i in range(1,12):
 
-    docs = [
-        ["/home/lucas/Documents/data/TIP2017/ieee_tip2017_klt1024_1.png"],
-        # ["/home/lucas/Documents/data/TIP2017/ieee_tip2017_klt1024_2.png"],
-        # ["/home/lucas/Documents/data/TIP2017/ieee_tip2017_klt1024_3.png"],
-        # ["/home/lucas/Documents/data/TIP2017/ieee_tip2017_klt1024_4.png"],
-        # ["/home/lucas/Documents/data/TIP2017/ieee_tip2017_klt1024_5.png"],
-        # ["/home/lucas/Documents/data/TIP2017/ieee_tip2017_klt1024_6.png"],
-        # ["/home/lucas/Documents/data/TIP2017/ieee_tip2017_klt1024_7.png"],
-        # ["/home/lucas/Documents/data/TIP2017/ieee_tip2017_klt1024_8.png"],
-        # ["/home/lucas/Documents/data/TIP2017/ieee_tip2017_klt1024_9.png"],
-        # ["/home/lucas/Documents/data/TIP2017/ieee_tip2017_klt1024_10.png"],
-        # ["/home/lucas/Documents/data/TIP2017/ieee_tip2017_klt1024_11.png"],
-    ]
+        exp_name = f"TIP2017_page{i}_lut_mean"
 
-    Ns = [26]
-    
-    learning_rates = [0.01]
+        docs = [
+            [f"/home/lucas/Documents/data/TIP2017/ieee_tip2017_klt1024_{i}.png"]
+        ]
 
-    central_tendencies = ["mean"]
+        Ns = [26]
+        
+        learning_rates = [0.01]
 
-    labels = [
-        'ALUT', # LUTmean
-        r'APC $\lambda=10^{-2}$', # MLPlr=1e-02
-    ]
+        central_tendencies = ["mean"]
 
-    linestyles = [
-        "solid", # "LUTmean"
-        "dashed", # MLPlr=1e-02
-    ]
+        labels = [
+            'ALUT', # LUTmean
+            r'APC $\lambda=10^{-2}$', # MLPlr=1e-02
+        ]
 
-    colors = [
-        "g", # "LUTmean"
-        "b", # MLPlr=1e-02 
-    ]
+        linestyles = [
+            "solid", # "LUTmean"
+            "dashed", # MLPlr=1e-02
+        ]
 
-    legend_ncol = 1
+        colors = [
+            "g", # "LUTmean"
+            "b", # MLPlr=1e-02 
+        ]
 
-    ylim = [0.0, 1.0]
+        legend_ncol = 1
 
-    backward_adaptive_coding_experiment(
-        exp_name,docs,Ns,learning_rates,central_tendencies,colors,linestyles,labels,legend_ncol,ylim,
-        manual_th=0.4,full_page=False,page_shape = (1024,791)
-    )
+        ylim = [0.0, 1.0]
+
+        backward_adaptive_coding_experiment(
+            exp_name,docs,Ns,learning_rates,central_tendencies,colors,linestyles,labels,legend_ncol,ylim,
+            manual_th=0.4,full_page=False,page_shape = (1024,791)
+        )
