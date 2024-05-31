b520b7c3437fdb99306481afb826f4c15d633eaf
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2024-05-31 11:49:41 -0300

    trying to obtain better linestyles

--- perceptronac/loading_and_saving.py
+++ perceptronac/loading_and_saving.py
@@ -34,11 +34,11 @@ linestyle_tuple = OrderedDict({
      'dashed':                'dashed', # Same as '--'
      'dashdot':               'dashdot', # Same as '-.'
      'loosely dotted':        (0, (1, 10)),
-    #  'dotted':                (0, (1, 1)),
+     'dotted2':                (0, (1, 1)),
      'densely dotted':        (0, (1, 1)),
      'long dash with offset': (5, (10, 3)),
      'loosely dashed':        (0, (5, 10)),
-    #  'dashed':                (0, (5, 5)),
+     'dashed2':                (0, (5, 5)),
      'densely dashed':        (0, (5, 1)),
      'loosely dashdotted':    (0, (3, 10, 1, 10)),
      'dashdotted':            (0, (3, 5, 1, 5)),
