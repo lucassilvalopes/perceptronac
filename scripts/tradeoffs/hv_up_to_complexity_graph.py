d694b69b5e3bdcfd9a15d89d29bd13bed40bac30
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2024-06-17 00:32:06 -0300

    fix in the up to complexity graphs

--- scripts/tradeoffs/hv_up_to_complexity_graph.py
+++ scripts/tradeoffs/hv_up_to_complexity_graph.py
@@ -87,7 +87,8 @@ def hv_vs_n_trained_networks(
         if up_to_complexity:
             filt_data = data[data[metrics[0].name] <= th]
 
-            ref_point = filt_data[[metric.name for metric in metrics]].max().values * 1.1
+            # ref_point = filt_data[[metric.name for metric in metrics]].max().values * 1.1
+            ref_point = data[[metric.name for metric in metrics]].max().values * 1.1
             optimization_config = build_optimization_config_mohpo(metrics,ref_point)
 
             ith_max_hv = get_hv_from_df(search_space,optimization_config,filt_data,label_to_params_func)
@@ -181,49 +182,47 @@ def adjust_ax_glch_comparison_mohpo(
 
 # In[7]:
 
+for complexity_axis in ["energy_noisy","params"]:
 
-# # complexity_axis = "energy_noisy"
-# complexity_axis = "params"
-
-# all_hvs_df = adjust_ax_glch_comparison_mohpo(
-#     ax_results_folder = "/home/lucas/Documents/perceptronac/scripts/tradeoffs/ax_results_energy_params_bits/",
-#     data_csv_path = "/home/lucas/Documents/perceptronac/scripts/tradeoffs/rate-noisy-joules-time-params_hx-10-20-40-80-160-320-640.csv",
-#     setup_func = partial(rc_setup,complexity_axis=("micro_joules_per_pixel" if complexity_axis == "energy_noisy" else complexity_axis)),
-#     glch_csv_paths = {
-#         "c_angle_rule": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_angle_rule_constrained_rate_vs_{complexity_axis}_history.csv",
-#         "u_angle_rule": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_angle_rule_unconstrained_rate_vs_{complexity_axis}_history.csv",
-#         "c_gift_wrapping": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_gift_wrapping_constrained_rate_vs_{complexity_axis}_history.csv",
-#         "u_gift_wrapping": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_gift_wrapping_unconstrained_rate_vs_{complexity_axis}_history.csv",
-#         "c_tie_break": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_tie_break_constrained_rate_vs_{complexity_axis}_history.csv"
-#     },
-#     read_glch_data_func = rc_read_glch_data,
-#     label_to_params_func = rc_label_to_params,
-#     params_to_label_func = rc_params_to_label,
-#     up_to_complexity=True
-# )
+    all_hvs_df = adjust_ax_glch_comparison_mohpo(
+        ax_results_folder = "/home/lucas/Documents/perceptronac/scripts/tradeoffs/ax_results_energy_params_bits/",
+        data_csv_path = "/home/lucas/Documents/perceptronac/scripts/tradeoffs/rate-noisy-joules-time-params_hx-10-20-40-80-160-320-640.csv",
+        setup_func = partial(rc_setup,complexity_axis=("micro_joules_per_pixel" if complexity_axis == "energy_noisy" else complexity_axis)),
+        glch_csv_paths = {
+            "c_angle_rule": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_angle_rule_constrained_rate_vs_{complexity_axis}_history.csv",
+            "u_angle_rule": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_angle_rule_unconstrained_rate_vs_{complexity_axis}_history.csv",
+            "c_gift_wrapping": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_gift_wrapping_constrained_rate_vs_{complexity_axis}_history.csv",
+            "u_gift_wrapping": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_gift_wrapping_unconstrained_rate_vs_{complexity_axis}_history.csv",
+            "c_tie_break": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_tie_break_constrained_rate_vs_{complexity_axis}_history.csv"
+        },
+        read_glch_data_func = rc_read_glch_data,
+        label_to_params_func = rc_label_to_params,
+        params_to_label_func = rc_params_to_label,
+        up_to_complexity=True
+    )
 
 
 # In[8]:
 
 
-# from ax_experiments_functions import rb_setup,rb_read_glch_data,rb_label_to_params, rb_params_to_label
+from ax_experiments_functions import rb_setup,rb_read_glch_data,rb_label_to_params, rb_params_to_label
 
-# all_hvs_df = adjust_ax_glch_comparison_mohpo(
-#     ax_results_folder="/home/lucas/Documents/perceptronac/scripts/tradeoffs/ax_results_energy_params_bits/",
-#     data_csv_path = "/home/lucas/Documents/perceptronac/scripts/tradeoffs/rate-model-bits_hx-10-20-40-80-160-320-640_b-8-16-32.csv",
-#     setup_func=rb_setup,
-#     glch_csv_paths = {
-#         "c_angle_rule": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_angle_rule_constrained_model_bits_vs_data_bits_history.csv",
-#         "u_angle_rule": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_angle_rule_unconstrained_model_bits_vs_data_bits_history.csv",
-#         "c_gift_wrapping": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_gift_wrapping_constrained_model_bits_vs_data_bits_history.csv",
-#         "u_gift_wrapping": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_gift_wrapping_unconstrained_model_bits_vs_data_bits_history.csv",
-#         "c_tie_break": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_tie_break_constrained_model_bits_vs_data_bits_history.csv"
-#     },
-#     read_glch_data_func=rb_read_glch_data,
-#     label_to_params_func=rb_label_to_params,
-#     params_to_label_func = rb_params_to_label,
-#     up_to_complexity=True
-# )
+all_hvs_df = adjust_ax_glch_comparison_mohpo(
+    ax_results_folder="/home/lucas/Documents/perceptronac/scripts/tradeoffs/ax_results_energy_params_bits/",
+    data_csv_path = "/home/lucas/Documents/perceptronac/scripts/tradeoffs/rate-model-bits_hx-10-20-40-80-160-320-640_b-8-16-32.csv",
+    setup_func=rb_setup,
+    glch_csv_paths = {
+        "c_angle_rule": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_angle_rule_constrained_model_bits_vs_data_bits_history.csv",
+        "u_angle_rule": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_angle_rule_unconstrained_model_bits_vs_data_bits_history.csv",
+        "c_gift_wrapping": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_gift_wrapping_constrained_model_bits_vs_data_bits_history.csv",
+        "u_gift_wrapping": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_gift_wrapping_unconstrained_model_bits_vs_data_bits_history.csv",
+        "c_tie_break": "/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch2D_tie_break_constrained_model_bits_vs_data_bits_history.csv"
+    },
+    read_glch_data_func=rb_read_glch_data,
+    label_to_params_func=rb_label_to_params,
+    params_to_label_func = rb_params_to_label,
+    up_to_complexity=True
+)
 
 
 # In[1]:
@@ -231,50 +230,25 @@ def adjust_ax_glch_comparison_mohpo(
 
 from ax_experiments_functions import rdc_setup,rdc_read_glch_data,rdc_label_to_params, rdc_params_to_label
 
-# complexity_axis = "params"
-complexity_axis = "flops"
 
+for complexity_axis in ["params","flops"]:
 
-all_hvs_df = adjust_ax_glch_comparison_mohpo(
-    ax_results_folder=f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/ax_results_rdc_{complexity_axis}",
-    data_csv_path = "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
-    setup_func=partial(rdc_setup,complexity_axis=complexity_axis),
-    glch_csv_paths = {
-        "c_angle_rule": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_angle_rule_constrained_bpp_loss_vs_mse_loss_vs_{complexity_axis}_start_left_threed_history.csv",
-        "u_angle_rule": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_angle_rule_unconstrained_bpp_loss_vs_mse_loss_vs_{complexity_axis}_start_left_threed_history.csv",
-        "c_gift_wrapping": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_gift_wrapping_constrained_bpp_loss_vs_mse_loss_vs_{complexity_axis}_start_left_threed_history.csv",
-        "u_gift_wrapping": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_gift_wrapping_unconstrained_bpp_loss_vs_mse_loss_vs_{complexity_axis}_start_left_threed_history.csv",
-        "c_tie_break": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_tie_break_constrained_bpp_loss_vs_mse_loss_vs_{complexity_axis}_start_left_threed_history.csv"
-    },
-    read_glch_data_func=partial(rdc_read_glch_data,complexity_axis=complexity_axis),
-    label_to_params_func=rdc_label_to_params,
-    params_to_label_func = rdc_params_to_label,
-    up_to_complexity=True
-)
-
-
-# In[ ]:
-
-
-# ( - all_hvs_df.drop("max_hv",axis=1).sub(all_hvs_df["max_hv"],axis=0)).plot(
-#     xlabel="number of observations", ylabel="Hypervolume Difference")
-
-
-# In[ ]:
-
-
-# all_hvs_df.plot(
-#     xlabel="number of observations", ylabel="Hypervolume")
-
-
-# In[ ]:
-
-
-# (all_hvs_df["max_hv"].iloc[0] - all_hvs_df.drop("max_hv",axis=1)).map(np.log10).plot(
-#     xlabel="number of observations", ylabel="Log Hypervolume Difference")
-
-
-# In[ ]:
+    all_hvs_df = adjust_ax_glch_comparison_mohpo(
+        ax_results_folder=f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/ax_results_rdc_{complexity_axis}",
+        data_csv_path = "/home/lucas/Documents/perceptronac/scripts/tradeoffs/bpp-mse-psnr-loss-flops-params_bmshj2018-factorized_10000-epochs_D-3-4_L-2e-2-1e-2-5e-3_N-32-64-96-128-160-192-224_M-32-64-96-128-160-192-224-256-288-320.csv",
+        setup_func=partial(rdc_setup,complexity_axis=complexity_axis),
+        glch_csv_paths = {
+            "c_angle_rule": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_angle_rule_constrained_bpp_loss_vs_mse_loss_vs_{complexity_axis}_start_left_threed_history.csv",
+            "u_angle_rule": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_angle_rule_unconstrained_bpp_loss_vs_mse_loss_vs_{complexity_axis}_start_left_threed_history.csv",
+            "c_gift_wrapping": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_gift_wrapping_constrained_bpp_loss_vs_mse_loss_vs_{complexity_axis}_start_left_threed_history.csv",
+            "u_gift_wrapping": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_gift_wrapping_unconstrained_bpp_loss_vs_mse_loss_vs_{complexity_axis}_start_left_threed_history.csv",
+            "c_tie_break": f"/home/lucas/Documents/perceptronac/scripts/tradeoffs/glch_results/glch3D_tie_break_constrained_bpp_loss_vs_mse_loss_vs_{complexity_axis}_start_left_threed_history.csv"
+        },
+        read_glch_data_func=partial(rdc_read_glch_data,complexity_axis=complexity_axis),
+        label_to_params_func=rdc_label_to_params,
+        params_to_label_func = rdc_params_to_label,
+        up_to_complexity=True
+    )
 
 
 
