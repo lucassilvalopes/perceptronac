acace7dea5b0ddb4c7fc9023d1d32a80b74e9fd8
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2024-07-28 13:33:56 -0300

    one more script

--- scripts/adaptive_perceptron_coding/bkwd_adpt_cdng_tip2017.py
+++ scripts/adaptive_perceptron_coding/bkwd_adpt_cdng_tip2017.py
@@ -7,8 +7,6 @@ if __name__ == "__main__":
 
     for i in range(1,12):
 
-        exp_name = f"TIP2017_page{i}_lut_mean"
-
         docs = [
             [f"/home/lucas/Documents/data/TIP2017/ieee_tip2017_klt1024_{i}.png"]
         ]
@@ -19,26 +17,7 @@ if __name__ == "__main__":
 
         central_tendencies = ["mean"]
 
-        labels = [
-            'ALUT', # LUTmean
-            r'APC $\lambda=10^{-2}$', # MLPlr=1e-02
-        ]
-
-        linestyles = [
-            "solid", # "LUTmean"
-            "dashed", # MLPlr=1e-02
-        ]
-
-        colors = [
-            "g", # "LUTmean"
-            "b", # MLPlr=1e-02 
-        ]
-
-        legend_ncol = 1
-
-        ylim = [0.0, 1.0]
-
         backward_adaptive_coding_experiment(
-            exp_name,docs,Ns,learning_rates,central_tendencies,colors,linestyles,labels,legend_ncol,ylim,
+            docs,Ns,learning_rates,central_tendencies,
             manual_th=0.4,full_page=False,page_shape = (1024,791)
         )
