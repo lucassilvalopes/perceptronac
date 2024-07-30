af73189530967e06af7fae00f65303d36b029e33
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2024-07-30 00:24:10 -0300

    testing apc pre-trained weights initialization

--- perceptronac/scripts/online_coding/bkwd_adpt_cdng_5pages.py
+++ perceptronac/scripts/online_coding/bkwd_adpt_cdng_5pages.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
 
     docs = [ # docs[i,j] = the path to the j'th page from the i'th document
         [
-            "/home/lucaslopes/perceptronac/SPL2021all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_1.png",
-            "/home/lucaslopes/perceptronac/SPL2021all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_2.png",
-            "/home/lucaslopes/perceptronac/SPL2021all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_3.png",
-            "/home/lucaslopes/perceptronac/SPL2021all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_4.png",
-            "/home/lucaslopes/perceptronac/SPL2021all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_5.png",
+            "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_1.png",
+            "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_2.png",
+            "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_3.png",
+            "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_4.png",
+            "/home/lucas/Documents/data/SPL2021/all_pages/Adaptive_Detection_of_Dim_Maneuvering_Targets_in_Adjacent_Range_Cells_5.png",
         ]
     ]
 
@@ -19,4 +19,6 @@ if __name__ == "__main__":
 
     central_tendencies = ["mean"] 
 
-    backward_adaptive_coding_experiment(docs,Ns,learning_rates,central_tendencies)
+    init_method = "/home/lucas/Documents/perceptronac/results/exp_1714238871/exp_1714238871_026_min_valid_loss_model.pt"
+
+    backward_adaptive_coding_experiment(docs,Ns,learning_rates,central_tendencies,init_method=init_method)
